c5f882caff9b133dd84027aff88a303d4a705d75
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-04-06 10:42:00 -0300

    Pergunta 'Quantas cartas?' implementada

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function perguntarQtdCartas() {
+
+    let verificador = 0;
+
+while (verificador === 0) {
+    let qtdCartas = prompt("Com quantas cartas você quer jogar? (Número par entre 4 e 14)");
+    if (qtdCartas >= 4 && qtdCartas <= 14 && ((qtdCartas % 2) == 0)) {
+        verificador = 1;
+    } else {
+        alert("Número inválido!");
+    }
+}
+}
+
+let verificador = 0;
+
+while (verificador === 0) {
+    let qtdCartas = prompt("Com quantas cartas você quer jogar? (Número par entre 4 e 14)");
+    if (qtdCartas >= 4 && qtdCartas <= 14 && ((qtdCartas % 2) == 0)) {
+        verificador = 1;
+    } else {
+        alert("Número inválido!");
+    }
+}
